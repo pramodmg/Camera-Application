72fd6bdf267ed854d5f632220737c49ab941c3f8
unknown <MI0055@EN-PRAMODMG.mynt.myntra.com>
2015-04-14 16:28:06 +0530

    added back functionalities

--- app/src/main/java/com/example/mi0055/simpletry/MainActivity.java
+++ app/src/main/java/com/example/mi0055/simpletry/MainActivity.java
@@ -10,6 +10,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -109,6 +110,22 @@ public class MainActivity extends ActionBarActivity {
         return image;
     }
 
+    private static final int TIME_INTERVAL = 2000; // # milliseconds, desired time passed between two back presses.
+    private long mBackPressed;
+
+    @Override
+    public void onBackPressed()
+    {
+        if (mBackPressed + TIME_INTERVAL > System.currentTimeMillis())
+        {
+            super.onBackPressed();
+            return;
+        }
+        else { Toast.makeText(getBaseContext(), "Tap back button in order to exit", Toast.LENGTH_SHORT).show(); }
+
+        mBackPressed = System.currentTimeMillis();
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -117,18 +134,19 @@ public class MainActivity extends ActionBarActivity {
         if (requestCode == 1) {
             viewImage.setImageURI(Uri.fromFile(new File(mpath)));
         } else if (requestCode == 2) {
-            Uri selectedImage = data.getData();
-            String[] filePath = { MediaStore.Images.Media.DATA };
-            Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
-            c.moveToFirst();
-            int columnIndex = c.getColumnIndex(filePath[0]);
-            String picturePath = c.getString(columnIndex);
-            c.close();
-            bitmap = (BitmapFactory.decodeFile(picturePath));
-            //Log.w("path of image from gallery......******************.........", picturePath + "");
-            viewImage.setImageBitmap(bitmap);
+            if (data != null) {
+                Uri selectedImage = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                bitmap = (BitmapFactory.decodeFile(picturePath));
+                //Log.w("path of image from gallery......******************.........", picturePath + "");
+                viewImage.setImageBitmap(bitmap);
+            }
         }
-
     }
 
     private void setPic() {
@@ -158,7 +176,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
